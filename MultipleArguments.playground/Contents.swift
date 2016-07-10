8ccd32e4a7d09eb0a674de429817c9d9a97a8597
susanlovaglio <susan@flatironschool.com>
2016-07-10 00:28:29 -0400

    fin

---
+++ MultipleArguments.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func moonsOf(planet: String, moons: Int){
+    
+    print("\(planet) has \(moons) moons orbiting it")
+}
+
+moonsOf("jupiter", moons: 67)
+
+moonsOf("mars", moons: 2)
+
